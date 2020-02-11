52eef1a616cec34341da0b51b88c3d5dea53a3ac
Florian Amsallem <florian.amsallem@epita.fr>
2020-02-11 23:31:48 +0100

    Add dump

--- test_2019/main.cc
+++ test_2019/main.cc
@@ -1,18 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 #include "algo-gen.hh"
+#include "parse.hh"
 
 int main(void)
 {
-    auto score = [](const int& a) { return (double)a; };
+    using entity = std::vector<Photo>;
 
-    auto generation = []() { return 42; };
+    AlgoGen<entity>::score_f score = [](const entity&) { return 0.; };
 
-    auto mutation = [](const std::map<double, int>&) {
-        return std::vector<int>{};
+    AlgoGen<entity>::generation_f generation = []() { return Photo{}; };
+
+    AlgoGen<entity>::mutation_f mutation = [](const std::map<double, entity>&) {
+        return std::vector<entity>{};
     };
 
-    auto dump = [](const int&) {};
+    auto dump = [](const entity& e) {
+        std::ofstream file("res");
+        if (!file.is_open())
+            std::cerr << "Failed open res file" << std::endl;
+        file << e.size() << std::endl;
+        for (const auto& i : e)
+        {
+            if (i.ids_.second >= 0)
+                file << i.ids_.first << " " << i.ids_.second << '\n';
+            file << i.ids_.first << '\n';
+        }
+        file.close();
+    };
 
-    auto gen = AlgoGen<int>(score, generation, mutation, dump);
-    gen(50);
+    auto gen = AlgoGen<entity>(score, generation, mutation, dump);
+    std::cout << score(gen(50)) << std::endl;
     return 0;
 }
