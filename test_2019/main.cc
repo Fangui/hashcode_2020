97fecf5e31baac216dd3b7bbb0940831fc67bf58
Corentin Mounier <mounie_c@epita.fr>
2020-02-13 17:15:51 +0100

    implement mutation function

--- test_2019/main.cc
+++ test_2019/main.cc
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -15,8 +17,20 @@ int main(void)
         return std::vector<Photo>();
     };
 
-    AlgoGen<entity>::mutation_f mutation = [](const std::map<double, entity>&) {
-        return std::vector<entity>{};
+    AlgoGen<entity>::mutation_f mutation = [](const std::map<double, entity>& best) {
+        std::vector<entity> new_gen;
+        for (const auto& gen : best) {
+            auto new_entity = entity(gen.second);
+
+            for (auto i = 0u; i < 4; i++) {
+                size_t i_1 = std::rand() % new_entity.size();
+                size_t i_2 = std::rand() % new_entity.size();
+                std::swap(new_entity[i_1], new_entity[i_2]);
+            }
+
+            new_gen.push_back(new_entity);
+        }
+        return new_gen;
     };
 
     auto gen = AlgoGen<entity>(score, generation, mutation, dump);
